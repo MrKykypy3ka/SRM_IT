1a2cb94d654717bb4c3fcc78ec682ff8a4764aa9
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2023-01-07 02:29:11 +0900

    show form

--- forms/autoresationForm.py
+++ forms/autoresationForm.py
@@ -11,36 +11,47 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(200, 130)
-        self.singinButton = QtWidgets.QPushButton(Dialog)
-        self.singinButton.setGeometry(QtCore.QRect(40, 90, 111, 31))
+class Ui_autoresationForm(object):
+    def setupUi(self, autoresationForm):
+        autoresationForm.setObjectName("autoresationForm")
+        autoresationForm.resize(200, 160)
+        self.singinButton = QtWidgets.QPushButton(autoresationForm)
+        self.singinButton.setGeometry(QtCore.QRect(40, 120, 111, 31))
         self.singinButton.setObjectName("singinButton")
-        self.inputLogin = QtWidgets.QLineEdit(Dialog)
-        self.inputLogin.setGeometry(QtCore.QRect(10, 10, 181, 31))
+        self.inputLogin = QtWidgets.QLineEdit(autoresationForm)
+        self.inputLogin.setGeometry(QtCore.QRect(10, 30, 181, 31))
+        self.inputLogin.setInputMask("")
+        self.inputLogin.setText("")
         self.inputLogin.setObjectName("inputLogin")
-        self.inputPassword = QtWidgets.QLineEdit(Dialog)
-        self.inputPassword.setGeometry(QtCore.QRect(10, 50, 181, 31))
+        self.inputPassword = QtWidgets.QLineEdit(autoresationForm)
+        self.inputPassword.setGeometry(QtCore.QRect(10, 80, 181, 31))
+        self.inputPassword.setInputMask("")
         self.inputPassword.setObjectName("inputPassword")
+        self.label = QtWidgets.QLabel(autoresationForm)
+        self.label.setGeometry(QtCore.QRect(10, 10, 47, 13))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(autoresationForm)
+        self.label_2.setGeometry(QtCore.QRect(10, 60, 81, 21))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_2.setObjectName("label_2")
+        self.inputPassword.raise_()
+        self.singinButton.raise_()
+        self.inputLogin.raise_()
+        self.label.raise_()
+        self.label_2.raise_()
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(autoresationForm)
+        QtCore.QMetaObject.connectSlotsByName(autoresationForm)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, autoresationForm):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Авторизация"))
-        self.singinButton.setText(_translate("Dialog", "Войти"))
-        self.inputLogin.setInputMask(_translate("Dialog", "Логин"))
-        self.inputPassword.setInputMask(_translate("Dialog", "Пароль"))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
-    sys.exit(app.exec_())
+        autoresationForm.setWindowTitle(_translate("autoresationForm", "Авторизация"))
+        self.singinButton.setText(_translate("autoresationForm", "Войти"))
+        self.label.setText(_translate("autoresationForm", "Логин"))
+        self.label_2.setText(_translate("autoresationForm", "Пароль"))
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from PyQt5 import QtWidgets
+from forms.autoresationForm import Ui_autoresationForm
+
 def main():
-    print("Test")
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    autoresationForm = QtWidgets.QDialog()
+    ui = Ui_autoresationForm()
+    ui.setupUi(autoresationForm)
+    autoresationForm.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
