85095aafb2197ec2f4ee88697b660fc5e590b959
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2023-01-15 22:13:21 +0900

    autorisation

--- forms/UiA.py
+++ forms/UiA.py
@@ -2,8 +2,20 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from forms.mainForm import Ui_mainForm
 from forms.autoresationForm import Ui_autoresationForm
 from forms.UiM import UiM
+import sqlite3
 
 FormA, WindowA = uic.loadUiType("forms/autoresationForm.ui")
+db = sqlite3.connect('identifier.sqlite')
+sql = db.cursor( )
+
+def autoresation(login, password):
+    for value in sql.execute("SELECT login, password FROM masters"):
+        if value[0] == login and value[1] == password:
+            return True
+    return False
+
+def main():
+    pass
 
 class UiA(QtWidgets.QDialog, FormA):
     def __init__(self, parent=None):
@@ -14,12 +26,6 @@ class UiA(QtWidgets.QDialog, FormA):
         self.mainForm = UiM(self)
 
     def singinButtonPresed(self):
-        self.close()
-        self.mainForm.show()
-
-
-def autoresation(login, password):
-    pass
-
-def main():
-    pass
+        if autoresation(self.uia.inputLogin.text(), self.uia.inputPassword.text()):
+            self.close()
+            self.mainForm.show()
