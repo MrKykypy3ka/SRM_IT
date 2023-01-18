6d3b9893b30202a91c15262cae5c62112745b4c6
BesedinOleg <oleg.metro2012@yandex.ru>
2023-01-18 15:25:48 +0900

    del

--- forms/UiA.py
+++ forms/UiA.py
@@ -1,5 +1,4 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from forms.mainForm import Ui_mainForm
 from forms.autoresationForm import Ui_autoresationForm
 from forms.UiM import UiM
 import sqlite3
@@ -8,14 +7,17 @@ FormA, WindowA = uic.loadUiType("forms/autoresationForm.ui")
 db = sqlite3.connect('identifier.sqlite')
 sql = db.cursor()
 
+
 def autoresation(login, password):
     for value in sql.execute("SELECT login, password FROM masters"):
         if value[0] == login and value[1] == password:
             return True
     return False
 
+
 def main():
     pass
+
 
 class UiA(QtWidgets.QDialog, FormA):
     def __init__(self, parent=None):
@@ -23,9 +25,10 @@ class UiA(QtWidgets.QDialog, FormA):
         self.uia = Ui_autoresationForm()
         self.uia.setupUi(self)
         self.uia.singinButton.clicked.connect(self.singinButtonPresed)
-        self.mainForm = UiM(self)
+        self.uim = UiM(self)
 
     def singinButtonPresed(self):
         #if autoresation(self.uia.inputLogin.text(), self.uia.inputPassword.text()):
         self.close()
-        self.mainForm.show()
+        self.uim.show()
+        self.uim.setUser(self.uia.inputLogin.text(), 0)
