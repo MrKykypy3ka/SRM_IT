ddb7c8f235da48e09e455b38de1417f5f5104701
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2023-01-30 02:10:46 +0900

    release .exe

---
+++ тут .exe/forms/UiD.py
@@ -0,0 +1,106 @@
+from PyQt5 import QtCore, QtWidgets, uic
+from forms.addForm import Ui_addForm
+import sqlite3
+
+FormD, WindowD = uic.loadUiType("forms/addForm.ui")
+
+
+class UiD(QtWidgets.QDialog, FormD):
+    def __init__(self, parent=None):
+        super(UiD, self).__init__(parent)
+        self.uid = Ui_addForm()
+        self.uid.setupUi(self)
+        self.uid.spinBox.setEnabled(False)
+        self.setFixedSize(self.width(), self.height())
+        self.uid.addButton.clicked.connect(self.addButtonPresed)
+        self.uid.textEdit.textChanged.connect(self.textTextChanged)
+        self.uid.lineEdit_2.textChanged.connect(self.line2TextChanged)
+        self.uid.lineEdit_3.textChanged.connect(self.line3TextChanged)
+        self.db = sqlite3.connect('identifier.sqlite')
+        self.sql = self.db.cursor()
+
+    def addButtonPresed(self):
+        if self.uid.textEdit.toPlainText() == "":
+            self.uid.textEdit.setStyleSheet("QTextEdit {background-color: Salmon;}")
+        elif self.uid.lineEdit_3.text() == "":
+            self.uid.lineEdit_3.setStyleSheet("QLineEdit {background-color: Salmon;}")
+        elif self.uid.lineEdit_2.text() == "" or not self.uid.lineEdit_2.text().isdigit():
+            self.uid.lineEdit_2.setStyleSheet("QLineEdit {background-color: Salmon;}")
+        else:
+            self.addOrders()
+            self.close()
+
+    def loadForm(self):
+        self.uid.dateTimeEdit.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.uid.dateTimeEdit.setDisplayFormat("dd.MM.yyyy (hh:mm)")
+        self.uid.dateTimeEdit_2.setDateTime(QtCore.QDateTime.currentDateTime())
+        self.uid.dateTimeEdit_2.setDisplayFormat("dd.MM.yyyy (hh:mm)")
+        try:
+            if self.sql.execute("SELECT MAX(order_id) FROM orders").fetchone()[0] is None:
+                self.uid.spinBox.setValue(1)
+            else:
+                self.uid.spinBox.setValue(self.sql.execute("SELECT MAX(order_id) FROM orders").fetchone()[0] + 1)
+            for value in self.sql.execute("SELECT name FROM type_of_work"):
+                self.uid.comboBox.addItem(value[0])
+            for value in self.sql.execute("SELECT last_name FROM masters"):
+                self.uid.comboBox_2.addItem(value[0])
+            for value in self.sql.execute("SELECT name FROM completion_mark"):
+                self.uid.comboBox_3.addItem(value[0])
+        except sqlite3.Error as error:
+            print("Ошибка при работе с SQLite", error)
+        self.uid.lineEdit_3.setText("")
+        self.uid.lineEdit_2.setText("")
+        self.uid.textEdit.setText("")
+        self.uid.textEdit.setStyleSheet("QTextEdit {background-color: White;}")
+
+    def showEvent(self, event):
+        self.uid.comboBox.clear()
+        self.uid.comboBox_2.clear()
+        self.uid.comboBox_3.clear()
+        self.loadForm()
+
+    def textTextChanged(self):
+        if self.uid.textEdit.toPlainText() == "":
+            self.uid.textEdit.setStyleSheet("QTextEdit {background-color: Salmon;}")
+        else:
+            self.uid.textEdit.setStyleSheet("QTextEdit {background-color: White;}")
+
+    def line2TextChanged(self):
+        if self.uid.lineEdit_2.text() == "":
+            self.uid.lineEdit_2.setStyleSheet("QLineEdit {background-color: Salmon;}")
+        else:
+            self.uid.lineEdit_2.setStyleSheet("QLineEdit {background-color: White;}")
+
+    def line3TextChanged(self):
+        if self.uid.lineEdit_3.text() == "":
+            self.uid.lineEdit_3.setStyleSheet("QLineEdit {background-color: Salmon;}")
+        else:
+            self.uid.lineEdit_3.setStyleSheet("QLineEdit {background-color: White;}")
+
+    def addOrders(self):
+        id = self.uid.spinBox.value()
+        customer = self.uid.lineEdit_3.text()
+        data_start = self.uid.dateTimeEdit.dateTime().toString('yyyy MM dd hh mm')
+        print(data_start)
+        data_end = self.uid.dateTimeEdit_2.dateTime().toString('yyyy MM dd hh mm')
+        price = self.uid.lineEdit_2.text()
+        notes = self.uid.textEdit.toPlainText()
+        try:
+            request = """SELECT type_of_work_id FROM type_of_work
+                                             WHERE name == (?);"""
+            type_work = self.sql.execute(request, (self.uid.comboBox.currentText(),)).fetchone()[0]
+            request = """SELECT masters_id FROM masters
+                                             WHERE last_name == (?);"""
+            master = self.sql.execute(request, (self.uid.comboBox_2.currentText(),)).fetchone()[0]
+            request = """SELECT completion_mark_id FROM completion_mark
+                                             WHERE name == (?);"""
+            mark = self.sql.execute(request, (self.uid.comboBox_3.currentText(),)).fetchone()[0]
+            sqlite_insert_query = """INSERT INTO orders
+                                      (order_id, customer, type_of_work_id, master_id, date_start, date_end, completion_mark_id, price, notes)
+                                      VALUES
+                                      (?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+            data = (id, customer, type_work, master, data_start, data_end, mark, price, notes)
+            self.sql.execute(sqlite_insert_query, data)
+            self.db.commit()
+        except sqlite3.Error as error:
+            print("Ошибка при работе с SQLite", error)
---
+++ тут .exe/forms/UiM.py
@@ -0,0 +1,175 @@
+from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox
+from forms.mainForm import Ui_mainForm
+from forms.UiU import UiU
+from forms.UiD import UiD
+from forms.UiV import UiV
+from forms.UiE import UiE
+import sqlite3
+
+
+FormM, WindowM = uic.loadUiType("forms/mainForm.ui")
+db = sqlite3.connect('identifier.sqlite')
+sql = db.cursor()
+
+
+class MouseTracker(QtCore.QObject):
+    positionChanged = QtCore.pyqtSignal(QtCore.QPoint)
+
+    def __init__(self, widget):
+        super().__init__(widget)
+        self._widget = widget
+        self.widget.setMouseTracking(True)
+        self.widget.installEventFilter(self)
+
+    @property
+    def widget(self):
+        return self._widget
+
+    def eventFilter(self, o, e):
+        if o is self.widget and e.type() == QtCore.QEvent.MouseMove:
+            self.positionChanged.emit(e.pos())
+        return super().eventFilter(o, e)
+
+
+class UiM(QtWidgets.QDialog, FormM):
+    def __init__(self, parent=None):
+        super(UiM, self).__init__(parent)
+        self.uim = Ui_mainForm()
+        self.uim.setupUi(self)
+        self.setFixedSize(self.width(), self.height())
+        self.uim.addButton.clicked.connect(self.addButtonPresed)
+        self.uim.deleteButton.clicked.connect(self.deleteButtonPresed)
+        self.uim.editButton.clicked.connect(self.editButtonPresed)
+        self.uim.usersButton.clicked.connect(self.userButtonPresed)
+        self.uim.vievButton.clicked.connect(self.viewButtonPresed)
+        self.uim.infoButton.clicked.connect(self.infoButtonPresed)
+        self.uim.blindButton.clicked.connect(self.blitButtonPresed)
+        self.addForm = UiD(self)
+        self.userForm = UiU(self)
+        self.viewForm = UiV(self)
+        self.editForm = UiE(self)
+        self.trackerAdd = MouseTracker(self.uim.addButton)
+        self.trackerAdd.positionChanged.connect(self.on_positionChangedAdd)
+        self.trackerEdit = MouseTracker(self.uim.editButton)
+        self.trackerEdit.positionChanged.connect(self.on_positionChangedEdit)
+        self.trackerView = MouseTracker(self.uim.vievButton)
+        self.trackerView.positionChanged.connect(self.on_positionChangedView)
+        self.trackerDel = MouseTracker(self.uim.deleteButton)
+        self.trackerDel.positionChanged.connect(self.on_positionChangedDel)
+        self.trackerUser = MouseTracker(self.uim.usersButton)
+        self.trackerUser.positionChanged.connect(self.on_positionChangedUser)
+        self.trackerInfo = MouseTracker(self.uim.infoButton)
+        self.trackerInfo.positionChanged.connect(self.on_positionChangedInfo)
+        self.trackerLow = MouseTracker(self.uim.blindButton)
+        self.trackerLow.positionChanged.connect(self.on_positionChangedLow)
+        self.loadForm()
+
+    def addButtonPresed(self):
+        self.setEnabled(False)
+        self.addForm.show()
+        self.addForm.setEnabled(True)
+        if not self.addForm.exec_():
+            self.setEnabled(True)
+            self.uim.listWidget.clear()
+            self.loadForm()
+
+    def editButtonPresed(self):
+        if self.uim.listWidget.selectedItems():
+            self.setEnabled(False)
+            self.editForm.setData(self.uim.listWidget.selectedItems()[0].text())
+            self.editForm.show()
+            self.editForm.setEnabled(True)
+
+            if not self.editForm.exec_():
+                self.setEnabled(True)
+                self.uim.listWidget.clear()
+                self.loadForm()
+
+    def deleteButtonPresed(self):
+        if self.uim.listWidget.selectedItems():
+            msg = QMessageBox(self)
+            msg.setWindowTitle("Подтвердите удаление")
+            msg.setText("Вы точно хотите удалить запись?")
+            msg.setIcon(QMessageBox.Question)
+            msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+            msg.setInformativeText(self.uim.listWidget.selectedItems()[0].text())
+            x = msg.exec_()
+            print(x)
+            if x == 16384:
+                try:
+                    request = """DELETE FROM orders WHERE notes == (?);"""
+                    sql.execute(request, (self.uim.listWidget.selectedItems()[0].text(),))
+                    db.commit()
+                except sqlite3.Error as error:
+                    print("Ошибка при работе с SQLite", error)
+        self.loadForm()
+
+
+
+    def userButtonPresed(self):
+        self.setEnabled(False)
+        self.userForm.show()
+        self.userForm.setEnabled(True)
+        if not self.userForm.exec_():
+            self.setEnabled(True)
+            self.loadForm()
+
+    def viewButtonPresed(self):
+        if self.uim.listWidget.selectedItems():
+            self.setEnabled(False)
+            self.viewForm.setData(self.uim.listWidget.selectedItems()[0].text())
+            self.viewForm.show()
+            self.viewForm.setEnabled(True)
+            if not self.viewForm.exec_():
+                self.setEnabled(True)
+                self.uim.listWidget.clear()
+                self.loadForm()
+
+    def infoButtonPresed(self):
+        file = open("info.txt", "r", encoding="utf-8")
+        s = file.read()
+        defaultBtn = QMessageBox.NoButton
+        result = QMessageBox.question(self, "Руководство пользователя", s, QMessageBox.Close, defaultBtn)
+        file.close()
+
+    def blitButtonPresed(self):
+        defaultBtn = QMessageBox.NoButton
+        result = QMessageBox.question(self, "TODO", "Режим для слабовидящих", QMessageBox.Close, defaultBtn)
+
+    def loadForm(self):
+        self.uim.listWidget.clear()
+        for value in sql.execute("SELECT notes FROM orders"):
+            self.uim.listWidget.addItems(value)
+
+    @QtCore.pyqtSlot(QtCore.QPoint)
+    def on_positionChangedAdd(self, pos):
+        self.uim.label_2.setText("Добавить запись")
+
+    @QtCore.pyqtSlot(QtCore.QPoint)
+    def on_positionChangedEdit(self, pos):
+        self.uim.label_2.setText("Редактировать запить")
+
+    @QtCore.pyqtSlot(QtCore.QPoint)
+    def on_positionChangedDel(self, pos):
+        self.uim.label_2.setText("Удалить запить")
+
+    @QtCore.pyqtSlot(QtCore.QPoint)
+    def on_positionChangedView(self, pos):
+        self.uim.label_2.setText("Просмотреть запись")
+
+    @QtCore.pyqtSlot(QtCore.QPoint)
+    def on_positionChangedUser(self, pos):
+        self.uim.label_2.setText("Добавить пользователя")
+
+    @QtCore.pyqtSlot(QtCore.QPoint)
+    def on_positionChangedInfo(self, pos):
+        self.uim.label_2.setText("Руководство пользователя")
+
+    @QtCore.pyqtSlot(QtCore.QPoint)
+    def on_positionChangedLow(self, pos):
+        self.uim.label_2.setText("Режим для слабовидящих")
+
+    def setLogin(self, login, avatar):
+        self.uim.label.setText(login)
+
